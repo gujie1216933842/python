6e9fe527d416b391918322c43de95a66ee5007a7
gujie1216933842 <gujientsy@163.com>
2018-03-26 23:10:37 +0800

    django

--- tiantian/test1/models.py
+++ tiantian/test1/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class bookInfo(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField()
+
+
+class heroInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    content = models.CharField(max_length=1000)
+    book = models.ForeignKey(bookInfo)
--- tiantian/tiantian/settings.py
+++ tiantian/tiantian/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test1'
 ]
 
 MIDDLEWARE = [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'     #1.8版本之后   中文'zh-hans'  英文 'en-us'
 
 TIME_ZONE = 'UTC'
 
