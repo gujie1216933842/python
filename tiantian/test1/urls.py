41c4d7bce0465f4a1a957be4ab52fc9e66874e94
gujie1216933842 <gujientsy@163.com>
2018-03-31 10:49:40 +0800

    django

--- tiantian/test1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns= [
-    url(r'^$',views.index),
-    url(r'^index1$',views.index1),
-    url(r'^index2$',views.index2),
-    url(r'^index3$',views.index3),
-    url(r'^show$',views.index3)
-]
