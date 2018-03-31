8fe6877ea18e5955e58c78a56a24fa985cc7b443
gujie1216933842 <gujientsy@163.com>
2018-03-31 09:39:43 +0800

    django

--- tiantian/test1/urls.py
+++ tiantian/test1/urls.py
@@ -4,5 +4,6 @@ urlpatterns= [
     url(r'^$',views.index),
     url(r'^index1$',views.index1),
     url(r'^index2$',views.index2),
-    url(r'^index3$',views.index3)
+    url(r'^index3$',views.index3),
+    url(r'^show$',views.index3)
 ]
