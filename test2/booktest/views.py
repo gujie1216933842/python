fc051b7ea0e4dc82a024127ce3c96d624987ad46
gujie1216933842 <gujientsy@163.com>
2018-03-31 20:46:13 +0800

    django

--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+    list = BookInfo.bookinfo1.filter(heroinfo__hcontent__contains='八')
+    content = {'list':list}
+    return render(request,'booktest/index.html',content)
