2340f7a1ec2ed5f28a8c1c792d8113f847298196
gujie1216933842 <gujientsy@163.com>
2018-03-31 10:20:09 +0800

    django

---
+++ test1/booktest/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class booInfo(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField
+
+class heroInfo(models):
+    name = models.CharField(max_length=50)
+    gender = models.BooleanField()
+    content = models.CharField(max_length=1000)
+    #定义外键
+    book = models.ForeignKey(booInfo)
+
