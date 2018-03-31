6423cdab25374dc7ebe9f1b0725dde0a4d6d970a
gujie1216933842 <gujientsy@163.com>
2018-03-31 11:10:31 +0800

    django

--- test2/booktest/models.py
+++ test2/booktest/models.py
@@ -2,4 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class BookInfo(models.Model):
+    bname = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField()
+    bread = models.IntegerField(default=0)
+    bcomment=models.IntegerField(default=0)
+    isDelete = models.BooleanField(default=False)
+    class Mrta:
+        db_table = "bookinfo"
 
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=10)
+    hgender = models.BooleanField(default=True)
+    hcontent = models.CharField(max_length=1000)
+    isDelete = models.BooleanField(default=False)
+    book = models.ForeignKey(BookInfo)
+    
