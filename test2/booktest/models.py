e8e4302d22c23d647af75c51c388f4beca345ba6
gujie1216933842 <gujientsy@163.com>
2018-03-31 14:11:29 +0800

    django

--- test2/booktest/models.py
+++ test2/booktest/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-
 # Create your models here.
 '''
-自定义重写查询方法
+自定义模型管理器
 '''
+
+
 class BookInfoManager(models.Manager):
     def get_queryset(self):
         return super(BookInfoManager, self).get_queryset().filter(isDelete=False)
@@ -20,8 +21,21 @@ class BookInfo(models.Model):
     class Mrta:
         db_table = "bookinfo"
 
-    bookinfo1 = models.Manager()   #django自己生成的模型类对象
-    bookinfo2 = BookInfoManager()  #自定义的模型类对象
+    bookinfo1 = models.Manager()  # django自己生成的模型类对象
+    bookinfo2 = BookInfoManager()  # 自定义的模型类对象
+    '''
+    自定义模型类创建方法,类似与__init__,注意,不能用__init__
+    '''
+    @classmethod
+    def create_obj(cls, btitle, bpub_date):
+        obj = BookInfo()
+        obj.btitle = btitle
+        obj.bpub_date = bpub_date
+        obj.bread = 0
+        obj.bcomment = 0
+        obj.isDelete = False
+        return obj
+
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=10)
