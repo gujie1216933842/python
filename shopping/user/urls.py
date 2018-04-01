e7ee4512162bddede7930139980c7f3a4629c754
gujie1216933842 <gujientsy@163.com>
2018-04-02 07:12:42 +0800

    django

---
+++ shopping/user/urls.py
@@ -0,0 +1,10 @@
+#Author:Bob
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^login/$',views.user.login)
+
+
+
+]
--- shopping/user/views.py
+++ shopping/user/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class user():
+    def login(request):
+        render(request,'user/login')
