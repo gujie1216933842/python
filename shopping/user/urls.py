336d36dedcbccaccb2a3245af9b370d73f91e2e2
gujie1216933842 <gujientsy@163.com>
2018-04-04 20:44:14 +0800

    django

--- shopping/user/urls.py
+++ shopping/user/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'login/$',views.user.login)
+    url(r'login/$',views.user.login),
+    url(r'regiser/$',views.user.register)
 
 
 
--- shopping/user/views.py
+++ shopping/user/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 class user():
     def login(request):
         return render(request, 'user/login.html')
+
+    def register(request):
+        return render(request, 'user/register.html')
+
+
