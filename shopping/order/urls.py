6279eb3fe6a1531b778718a9e8fc7ab50ab8d114
gujie1216933842 <gujientsy@163.com>
2018-04-15 18:47:58 +0800

    django

--- shopping/order/urls.py
+++ shopping/order/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'user_center_order/$', views.order.index)
+    url(r'user_center_order/$', views.order.index),
+    url(r'user_center_site/$', views.order.address)
 ]
