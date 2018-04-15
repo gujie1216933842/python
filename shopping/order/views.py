731bad40e8b0427a48a6eae94e62154cf8897545
gujie1216933842 <gujientsy@163.com>
2018-04-15 18:14:31 +0800

    django

--- shopping/order/views.py
+++ shopping/order/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 class order():
     def index(request):
-        return render(request, 'order/user_center_order')
+        return render(request, 'order/user_center_order.html')
