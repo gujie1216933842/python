a457eebf7f2bf15cb20901839b7afb7008659475
gujie1216933842 <gujientsy@163.com>
2018-04-18 00:04:40 +0800

    django

--- shopping/cart/views.py
+++ shopping/cart/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 class cart():
     def index(request):
-        return render(request,'cart/cart.html')
+        return render(request, 'cart/cart.html', {'page_name': 1})
