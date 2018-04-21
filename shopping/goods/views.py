5832b60dd12ee9bc47f1507706e786bde30c62c3
gujie1216933842 <gujientsy@163.com>
2018-04-21 23:37:02 +0800

    django

--- shopping/goods/views.py
+++ shopping/goods/views.py
@@ -10,24 +10,23 @@ from . import models
 
 
 # 控制器中以类的方式
-class goods():
-    fun1 = ''
+class goods:
 
-    def __init__(self, request):
-        self.fun1 = goods
+    # def __init__(self, request):
+    #     self.fun1 = goods
 
-    def index(self,request):
+    def index(self, request):
         # 显示首页信息
         # 时令水果
         res1 = models.goods.objects.filter(delete_flag=0)
-        res1 = self.fun1.float_control(res1)
+        res1 = self.float_control(res1)
 
         return render(request, 'goods/index.html', {'guest_cart': 1, 'res1': res1})
 
-    def detail(request):
+    def detail(self, request):
         return render(request, 'goods/detail.html')
 
-    def float_control(res, decimal_count=2):
+    def float_control(self, res, decimal_count=2):
         for i in res:
             res[i] = round(res['price'], decimal_count)
         return res
