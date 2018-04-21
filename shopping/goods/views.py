11f8f8022102c1d4cd81357b1b8b734f7eb5c8f4
gujie1216933842 <gujientsy@163.com>
2018-04-21 17:10:37 +0800

    django

--- shopping/goods/views.py
+++ shopping/goods/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
+from . import models
+
 
 # Create your views here.
 
-#控制器中以方法的方式
+# 控制器中以方法的方式
 # def index(request):
 #     return  render(request,'goods/index.html')
 
 
-#控制器中以类的方式
+# 控制器中以类的方式
 class goods():
     def index(request):
-        return render(request,'goods/index.html',{'guest_cart':1})
+        # 显示首页信息
+        # 时令水果
+        res1 = models.goods.objects.fliter(delete_flag=0)
+        return render(request, 'goods/index.html', {'guest_cart': 1, 'res1': res1})
 
     def detail(request):
-        return render(request,'goods/detail.html')
-
-
-
-
-
-
-
+        return render(request, 'goods/detail.html')
