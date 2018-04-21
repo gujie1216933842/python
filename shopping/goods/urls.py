c1ec88d6546fe4d2450c9d186affe746c8e297c5
gujie1216933842 <gujientsy@163.com>
2018-04-21 23:03:05 +0800

    django

--- shopping/goods/urls.py
+++ shopping/goods/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.goods.index),
+    url(r'^$', views.goods.index()),
     url(r'^detail/$', views.goods.detail),
 
 
