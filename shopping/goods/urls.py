f07211e188c8092b6c77782fc219e95fc5ffc22e
gujie1216933842 <gujientsy@163.com>
2018-04-22 21:04:40 +0800

    django

--- shopping/goods/urls.py
+++ shopping/goods/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.goodsIndex.as_view()),
+    url(r'^$', views.goodsIndex.as_view()),  #as_view()括号一定要加上
     url(r'^detail/$', views.goodsDatail.as_view()),
 
 
