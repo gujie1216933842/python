7ef83e3fcd5e8456c3c87c3e997bc6887151e234
gujie1216933842 <gujientsy@163.com>
2018-05-10 07:48:59 +0800

    django

--- shopping/good/models.py
+++ shopping/good/models.py
@@ -7,7 +7,9 @@ class good(models.Model):
     # 定义商品字段
     name = models.CharField(max_length=20, null=False , default="")
     price = models.FloatField(null=False,default='')
-    pic = models.CharField(max_length=50, null=False ,default="")
+    #pic = models.CharField(max_length=50, null=False ,default="")
+    pic = models.ImageField(upload_to='/statics/gooodsImage')
+
     detail_pic = models.CharField(max_length=50, null=False ,default="")
     unit = models.CharField(max_length=20, null=False ,default="")
     raw_add_time = models.DateTimeField(auto_now_add=True)
