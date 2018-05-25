bb25a10b485212fa44750b21d3c6458c61884b69
gujie1216933842 <gujientsy@163.com>
2018-05-25 23:36:15 +0800

    django

--- shopping/good/urls.py
+++ shopping/good/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.goodsIndex.as_view()),  #as_view()括号一定要加上
     url(r'^detail/$', views.goodsDatail.as_view()),
-    url(r'^send/$', views.send.as_view()),
+    url(r'^send/$', views.send),
 
 
 ]
