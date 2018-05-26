e1452b6d996080d58fb2abaa5baf79e948be6c1c
gujie1216933842 <gujientsy@163.com>
2018-05-26 23:13:01 +0800

    notedjango

--- shopping/good/urls.py
+++ shopping/good/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.goodsIndex.as_view()),  #as_view()括号一定要加上
     url(r'^detail/$', views.goodsDatail.as_view()),
-    url(r'^send/$', views.send),
 
 
 ]
