855e3ecd0ab67084b852e84dccdc2dce79041853
gujie1216933842 <gujientsy@163.com>
2018-06-02 01:00:25 +0800

    'aa'

--- celerytest/hello/models.py
+++ celerytest/hello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Add(models.Model):
+    task_id = models.CharField(max_length=128)
+    first = models.IntegerField()  # 存储第一个加数
+    second = models.IntegerField()  # 存储第二个加数
+    log_date = models.DateTimeField()  # 存储开始时间
