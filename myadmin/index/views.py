7bfe92e799b0c75c65b889dee3a7de6906420bfe
gujie1216933842 <gujientsy@163.com>
2018-06-09 17:13:49 +0800

    'aa'

--- myadmin/index/views.py
+++ myadmin/index/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.views.generic.base import View
+
+
 # Create your views here.
 
 class IndexHandler(View):
-    def get(self,request):
-        HttpResponse('ok')
-        #render(request,'index/index.html')
-
+    def get(self, request):
+        return HttpResponse('ok')
+        # render(request,'index/index.html')
