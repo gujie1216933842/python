271969fc55e49f951739fe30cc338fb0d37aaf44
gujie1216933842 <gujientsy@163.com>
2018-06-10 00:51:48 +0800

    note

--- myadmin/user/views.py
+++ myadmin/user/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.views.generic.base import View
-import os,sys
-#找到当前目录的路径
+import os, sys
+
+# 找到当前目录的路径
 file_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-sys.path.insert(0,file_path)
+sys.path.insert(0, file_path)
 from utils.captcha.captcha import Verifycode
 
+
 class Logout(View):
-    def get(self,request):
-        return render(request,'user/login.html')
+    def get(self, request):
+        return render(request, 'user/login.html')
 
 
 class Captcha(View):
     def get(self):
-        captcha = Verifycode()
+        captcha = Verifycode(200, 40, 4)
         return HttpResponse(captcha)
-
-
