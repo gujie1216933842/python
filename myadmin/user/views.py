ebd42e5ad415bf777e6d766a851afa7b04e9886b
gujie1216933842 <gujientsy@163.com>
2018-06-09 23:51:26 +0800

    django

--- myadmin/user/views.py
+++ myadmin/user/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render,HttpResponse
 from django.views.generic.base import View
+from utils.captcha.captcha import Verifycode
+
 
 class Logout(View):
     def get(self,request):
         return render(request,'user/login.html')
+
+
+
+
+
+
+
+
+class Captcha(View):
+    captcha = Verifycode()
+    HttpResponse(captcha)
+
 
