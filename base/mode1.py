215e79740a58fa8ecab6eb923034c52e6c8c5d15
gujie1216933842 <gujientsy@163.com>
2018-06-12 23:19:06 +0800

    'aa'

--- base/mode1.py
+++ base/mode1.py
@@ -1,7 +1,5 @@
-
-from myadmin.myadmin.settings import DATABASES
 import pymysql
-
+from base import settings
 
 class Base(object):
     _instance = None
@@ -15,11 +13,11 @@ class Base(object):
 
     def __init__(self):
         self._conn = pymysql.Connect(
-            host=DATABASES['default']['HOST'],
-            port=int(DATABASES['default']['PORT']),
-            user=DATABASES['default']['USER'],
-            passwd=DATABASES['default']['PASSWORD'],
-            db=DATABASES['default']['NAME'],
+            host=settings.DATABASES['default']['HOST'],
+            port=int(settings.DATABASES['default']['PORT']),
+            user=settings.DATABASES['default']['USER'],
+            passwd=settings.DATABASES['default']['PASSWORD'],
+            db=settings.DATABASES['default']['NAME'],
             charset='utf8'
         )
         self._cursor = self._conn.cursor()  # 最终返回数据类型元组
