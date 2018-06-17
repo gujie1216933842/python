a3ffb516295a739d82980a2abceb69f4d5ae5e60
gujie1216933842 <gujientsy@163.com>
2018-06-17 22:51:51 +0800

    'aa'

--- shopping/user/models.py
+++ shopping/user/models.py
@@ -3,14 +3,15 @@ from django.db import models
 # Create your models here.
 
 
-Type = {
-    '1': '新',
-    '2': '老',
-    '3': '外联用户',
-}
 
 
 class UserInfo(models.Model):
+    Type = {
+        '1': '新',
+        '2': '老',
+        '3': '外联用户',
+    }
+
     # 以下数据库的字段定义一定要加
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
