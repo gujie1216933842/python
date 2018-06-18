40afaa21499f423de4dee7014da119ace7b8c811
gujie1216933842 <gujientsy@163.com>
2018-06-18 08:08:47 +0800

    'aa'

--- myadmin/user/models.py
+++ myadmin/user/models.py
@@ -5,11 +5,15 @@ Type = {
     '2': '老',
     '3': '外联用户',
 }
-
+USER_CHOICES = (
+        (1, '新'),
+        (2, '老'),
+        (3, '外联'),
+    )
 class UserInfo(models.Model):
 
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    type = models.CharField(choices=Type)
+    type = models.CharField(choices=USER_CHOICES,max_length=10)
 
     raw_add_time = models.DateTimeField()
