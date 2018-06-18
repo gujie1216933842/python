44f2f8e172838c9f0f3ed529772def3e377a5fd8
gujie1216933842 <gujientsy@163.com>
2018-06-18 08:01:29 +0800

    'aa'

--- myadmin/user/models.py
+++ myadmin/user/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+Type = {
+    '1': '新',
+    '2': '老',
+    '3': '外联用户',
+}
+
 class UserInfo(models.Model):
-    Type = {
-        '1': '新',
-        '2': '老',
-        '3': '外联用户',
-    }
+
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    type = models.CharField(choices=Type, max_length=30)
+    type = models.CharField(choices=Type)
 
     raw_add_time = models.DateTimeField()
