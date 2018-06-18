4f56acf5c905669083d197ff577cd14c34b5a656
gujie1216933842 <gujientsy@163.com>
2018-06-18 22:43:33 +0800

    'aa'

--- myadmin/user/urls.py
+++ myadmin/user/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     url(r'logout/$',views.Logout.as_view()),
     url(r'login/$',views.Login.as_view()),
     url(r'captcha/$',views.Captcha.as_view()),
-    url(r'get_session/$',views.GetSession.as_view()),
-    url(r'set_session/$',views.SetSession.as_view()),
 
 
 ]
