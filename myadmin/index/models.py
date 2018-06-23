c5d3fe4be249146cea2981080cda2a3f8283688b
gujie1216933842 <gujientsy@163.com>
2018-06-23 20:42:19 +0800

    'aa'

--- myadmin/index/models.py
+++ myadmin/index/models.py
@@ -5,7 +5,15 @@ from django.db import models
 
 
 class resource(models.Model):
-    name = models.CharField(max_length=10, null=False, default='', db_column='资源名称')
-    link = models.CharField(max_length=50, null=False, default='', db_column='资源链接')
-    delete_flag = models.BooleanField(null=False, default=False, db_column='记录是否删除,0:否,1:删除')
-    raw_add_time = models.DateTimeField(auto_now_add=True, db_column='记录添加时间')
+    '''
+    default只是针对于用resource对象时,才有作用
+    migration的时候在数据库中迁移,字段后面不会出现default
+    '''
+    name = models.CharField(max_length=10, null=False, default='')
+    link = models.CharField(max_length=50, null=False, default='')
+    delete_flag = models.BooleanField(null=False, default=False)
+    raw_add_time = models.DateTimeField(auto_now_add=True, null=False)
+    raw_update_time = models.DateTimeField(auto_now=True)
+
+
+
