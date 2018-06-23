d22237049b348433b3ae2806ee322721508ec8ee
gujie1216933842 <gujientsy@163.com>
2018-06-23 16:01:19 +0800

    'aa'

--- myadmin/index/models.py
+++ myadmin/index/models.py
@@ -5,8 +5,7 @@ from django.db import models
 
 
 class resource(models.Model):
-    name = models.CharField(u'资源名称', default='', max_length=10, null=False)
-    link = models.CharField(u'资源连接', max_length=50, null=False, default='')
-    delete_flag = models.BooleanField(u'记录是否删除标记,0:否,1:是', null=False, default=True)
-    raw_add_time = models.DateTimeField(u'记录添加时间', auto_now_add=True)
-
+    name = models.CharField(max_length=10, null=False, default='', db_column='资源名称')
+    link = models.CharField(max_length=50, null=False, default='', db_column='资源链接')
+    delete_flag = models.BooleanField(null=False, default=False, db_column='记录是否删除,0:否,1:删除')
+    raw_add_time = models.DateTimeField(auto_now_add=True, db_column='记录添加时间')
