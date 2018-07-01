879e530c610a063b22dcfbcb2af33675e4d5111b
gujie1216933842 <gujientsy@163.com>
2018-07-01 13:27:04 +0800

    'aa'

--- myadmin/user/models.py
+++ myadmin/user/models.py
@@ -11,5 +11,5 @@ class UserInfo(models.Model):
     password = models.CharField(max_length=50)
     type = models.CharField(choices=USER_CHOICES,max_length=10)
 
-    raw_add_time = models.DateTimeField()
-    raw_update_time = models.DateTimeField()
+    raw_add_time = models.DateTimeField(auto_now_add=True)     #用于记录添加时间
+    raw_update_time = models.DateTimeField(auto_now=True)      #用于更新
