046c8a5d320b483e51b656fc1bd6ccb9503a1436
gujie1216933842 <gujientsy@163.com>
2018-07-01 14:05:33 +0800

    'aa'

--- myadmin/user/models.py
+++ myadmin/user/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-USER_CHOICES = (
-        (1, '新'),
-        (2, '老'),
-        (3, '外联'),
-    )
-class UserInfo(models.Model):
 
+class UserInfo(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    type = models.CharField(choices=USER_CHOICES,max_length=10)
-
-    raw_add_time = models.DateTimeField(auto_now_add=True)     #用于记录添加时间
-    raw_update_time = models.DateTimeField(auto_now=True)      #用于更新
+    raw_add_time = models.DateTimeField(auto_now_add=True)  # 用于记录添加时间
+    raw_update_time = models.DateTimeField(auto_now=True)  # 用于更新
