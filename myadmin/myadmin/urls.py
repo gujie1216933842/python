8abb782417e1747f58627cd1246709df43e3d92a
gujie1216933842 <gujientsy@163.com>
2018-07-01 23:00:25 +0800

    'aa'

--- myadmin/myadmin/urls.py
+++ myadmin/myadmin/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', include("index.urls")),
+    url(r'^/', include("index.urls")),
     url(r'^user/', include("user.urls")),
     url(r'^rediscache/', include("rediscache.urls")),
 ]
