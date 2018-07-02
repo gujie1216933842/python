31b53cdfd8874d720e30ac26988eb90934faf57f
gujientsy <gujientsy@163.com>
2018-07-02 16:58:24 +0800

    'aa'

--- myadmin/index/views.py
+++ myadmin/index/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic.base import View
+import os, sys
+file_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, file_path)
 from utils.common import require_logined
 
 # Create your views here.
