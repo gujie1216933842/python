2c2838bcefe3482086b385408b105267c3d22a8d
gujie1216933842 <gujientsy@163.com>
2018-07-03 23:01:38 +0800

    'aa'

--- myadmin/index/models.py
+++ myadmin/index/models.py
@@ -4,6 +4,30 @@ from django.db import models
 # Create your models here.
 
 
+class TopResorce(models.Model):
+    '''
+    default只是针对于用resource对象时,才有作用
+    migration的时候在数据库中迁移,字段后面不会出现default
+    资源管理表
+    '''
+    name = models.CharField(max_length=10, null=False, default='')
+    link = models.CharField(max_length=50, null=False, default='')
+    delete_flag = models.BooleanField(null=False, default=False)
+    raw_add_time = models.DateTimeField(auto_now_add=True, null=False)
+    raw_update_time = models.DateTimeField(auto_now=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Resource(models.Model):
     '''
     default只是针对于用resource对象时,才有作用
