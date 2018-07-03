f0a7a3072c763adaba36aa8f46d234ac69a3a8f7
gujie1216933842 <gujientsy@163.com>
2018-07-04 00:05:08 +0800

    'aa'

--- myadmin/index/models.py
+++ myadmin/index/models.py
@@ -2,30 +2,13 @@ from django.db import models
 
 
 # Create your models here.
-
-
-class TopResorce(models.Model):
-    '''
-    default只是针对于用resource对象时,才有作用
-    migration的时候在数据库中迁移,字段后面不会出现default
-    资源管理表
-    '''
-    name = models.CharField(max_length=10, null=False, default='')
-    link = models.CharField(max_length=50, null=False, default='')
-    delete_flag = models.BooleanField(null=False, default=False)
-    raw_add_time = models.DateTimeField(auto_now_add=True, null=False)
-    raw_update_time = models.DateTimeField(auto_now=True)
-
-
-
-
-
-
-
-
-
-
-
+type = (
+    (1, "系统管理"),
+    (2, "游戏管理"),
+    (3, "商城管理"),
+    (4, "爬虫管理"),
+    (5, "数据分析"),
+)
 
 
 class Resource(models.Model):
@@ -34,41 +17,11 @@ class Resource(models.Model):
     migration的时候在数据库中迁移,字段后面不会出现default
     资源管理表
     '''
-    name = models.CharField(max_length=10, null=False, default='')
+    name = models.CharField(max_length=50, null=False, default='')
     link = models.CharField(max_length=50, null=False, default='')
-    delete_flag = models.BooleanField(null=False, default=False)
-
-    raw_add_time = models.DateTimeField(auto_now_add=True, null=False)
-    raw_update_time = models.DateTimeField(auto_now=True)
-
-
-class Catalog(models.Model):
-    '''
-    目录管理表
-    '''
-    name = models.CharField(max_length=10, null=False, default='')
+    category = models.CharField(choices=type,null=False,)
     delete_flag = models.BooleanField(null=False, default=False)
     raw_add_time = models.DateTimeField(auto_now_add=True, null=False)
     raw_update_time = models.DateTimeField(auto_now=True)
 
 
-class ResourceCatalog(models.Model):
-    '''
-    资源和目录关系表
-    '''
-    resource_id = models.IntegerField(null=False, default=0)
-    catalog_id = models.IntegerField(null=False, default=0)
-    delete_flag = models.BooleanField(null=False, default=False)
-    raw_add_time = models.DateTimeField(auto_now_add=True, null=False)
-    raw_update_time = models.DateTimeField(auto_now=True)
-
-
-class UserResource(models.Model):
-    '''
-    资源和用户id关系表
-    '''
-    user_id = models.IntegerField(null=False, default=0)
-    resource_id = models.IntegerField(null=False, default=0)
-    delete_flag = models.BooleanField(null=False, default=False)
-    raw_add_time = models.DateTimeField(auto_now_add=True, null=False)
-    raw_update_time = models.DateTimeField(auto_now=True)
