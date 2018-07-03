81337716526f7f7170985ac70f0aed4e318c8475
gujie1216933842 <gujientsy@163.com>
2018-07-04 00:36:27 +0800

    'aa'

--- myadmin/index/models.py
+++ myadmin/index/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-type = (
-    (1, "系统管理"),
-    (2, "游戏管理"),
-    (3, "商城管理"),
-    (4, "爬虫管理"),
-    (5, "数据分析"),
-)
 
 
 class Resource(models.Model):
@@ -17,11 +10,16 @@ class Resource(models.Model):
     migration的时候在数据库中迁移,字段后面不会出现default
     资源管理表
     '''
+    type = (
+        (1, "系统管理"),
+        (2, "游戏管理"),
+        (3, "商城管理"),
+        (4, "爬虫管理"),
+        (5, "数据分析"),
+    )
     name = models.CharField(max_length=50, null=False, default='')
     link = models.CharField(max_length=50, null=False, default='')
-    category = models.PositiveIntegerField(choices=type,null=False,)
+    category = models.CharField(max_length=5, choices=type, default=1)
     delete_flag = models.BooleanField(null=False, default=False)
     raw_add_time = models.DateTimeField(auto_now_add=True)
     raw_update_time = models.DateTimeField(auto_now=True)
-
-
