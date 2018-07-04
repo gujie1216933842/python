b21b6c8536e1e177da4dd813aee35d04ae54b49f
gujie1216933842 <gujientsy@163.com>
2018-07-04 08:40:13 +0800

    ''aa

--- myadmin/index/views.py
+++ myadmin/index/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic.base import View
-from utils.common import require_logined
+from utils.common import require_logined, django_model_opration
+from . import models
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -24,8 +26,15 @@ class Source(View):
     def get(self, request):
         return render(request, 'index/source.html')
 
-
-
-
-
-
+    def post(self, request):
+        page = request.GET.get('page', 1)
+        rows = request.GET.get('rows', 10)
+        page = int(page)
+        rows = int(rows)
+        start = rows * (page - 1)
+        end = start + rows - 1
+        # 获取数据
+        sourceItems = models.Resource.objects.all()[start, end]
+        sourceList = django_model_opration(sourceItems)
+        count = models.Resource.objects.all().count()
+        return JsonResponse({"rows": sourceList, 'total': count})
