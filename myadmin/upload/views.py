ee3198a05b97a90f79208de7a4d0fc30eefa2d36
gujie1216933842 <gujientsy@163.com>
2018-07-08 10:46:54 +0800

    aa

--- myadmin/upload/views.py
+++ myadmin/upload/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.shortcuts import HttpResponse
 
 
 # Create your views here.
@@ -8,3 +9,6 @@ from django.views.generic import View
 class UploadExcelUserId(View):
     def get(self, rquest):
         return render(rquest, 'upload/exceluserid.html')
+
+    def post(self, request):
+        return HttpResponse('ok')
