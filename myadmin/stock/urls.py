b1f1772fb0b6c694ba8feb95b904f48f47e98c24
gujientsy <gujientsy@163.com>
2018-07-17 21:13:59 +0800

    aa

--- myadmin/stock/urls.py
+++ myadmin/stock/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'szlist/$', views.SzStockList.as_view()),
+    url(r'szchangelist/$', views.SzGgChangeStockList.as_view()),
 
 ]
--- myadmin/stock/views.py
+++ myadmin/stock/views.py
@@ -25,3 +25,26 @@ class SzStockList(View):
             ret[index] = value
 
         return JsonResponse({'rows': ret, 'total': total})
+
+
+
+
+class SzGgChangeStockList(View):
+    def get(self, request):
+        return render(request, 'stock/szchangelist.html')
+
+    def post(self, request):
+        page = request.POST.get('page', 1)
+        rows = request.POST.get('rows', 10)
+        company_code = request.POST.get('search_company_code', '')
+        name = request.POST.get('search_name', '')
+        SzStockList = mymodels.SzStock()
+        ret = SzStockList.getList(int(page), int(rows), company_code, name)
+        total = SzStockList.getCount(company_code, name)['n']
+        for index, value in enumerate(ret):
+            for k, v in value.items():
+                v = decimal_serializable(v)
+                value[k] = datetime_serializable(v)
+            ret[index] = value
+
+        return JsonResponse({'rows': ret, 'total': total})
