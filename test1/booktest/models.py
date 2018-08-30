2b33ca58d2a3457baba4579357e1de3c08d91ccf
gujie1216933842 <gujientsy@163.com>
2018-08-30 22:54:53 +0800

    db

--- test1/booktest/models.py
+++ test1/booktest/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
-class booInfo(models.Model):
-    title = models.CharField(max_length=20)
-    date = models.DateTimeField
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(db_column='pub_date')
+    bread = models.IntegerField(default=0)
+    bcomment = models.IntegerField(null=False)
+    isDelete = models.BooleanField(default=False)
 
-class heroInfo(models.Model):
-    name = models.CharField(max_length=50)
-    gender = models.BooleanField()
-    content = models.CharField(max_length=1000)
-    #定义外键
-    book = models.ForeignKey(booInfo,on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'bookinfo'
 
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=50)
+    hgender = models.BooleanField(default=True)
+    hcontent = models.CharField(max_length=1000)
+    isDelete = models.BooleanField(default=False)
+    # 定义外键
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
--- test1/test1/settings.py
+++ test1/test1/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ ALLOWED_HOSTS = [
     '47.97.165.75',
     '127.0.0.1'
 ]
-
 
 # Application definition
 
@@ -73,17 +71,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'test1.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -103,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -116,7 +119,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
