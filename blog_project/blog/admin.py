3b33af5490e057aa74f81a319ffe9d3ee0c9f05c
gujie1216933842 <gujientsy@163.com>
2018-09-01 20:46:05 +0800

    admin_regiser

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
