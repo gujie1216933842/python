d257d81aa1972a36950ce75b04f317500b43d11d
gujie1216933842 <gujientsy@163.com>
2018-09-01 10:04:04 +0800

    log

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+import logging
 
-
-# Create your views here.
+# 定义日志器
+logger = logging.getLogger('blog.views')
 
 
 def index(request):
+    try:
+        f = open('ss.txt', 'r')
+    except Exception as e:
+        logger.error(e)
+
     return render(request, 'blog/index.html')
