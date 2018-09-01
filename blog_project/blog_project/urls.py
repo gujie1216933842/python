26af4b10286562629209d6605a5043ea76815c33
gujie1216933842 <gujientsy@163.com>
2018-09-01 22:41:16 +0800

    url

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
-    url(r'^uploads/(?P<path>.*)$', 'django.views.static.serve', name={'document_root': settings.MEDIA_ROOT}),
+    url(r'^uploads/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
 ]
