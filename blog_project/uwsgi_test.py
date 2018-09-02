15f8af21c6b7b07654c8f875c17e5561e08bc218
gujie1216933842 <gujientsy@163.com>
2018-09-02 22:58:45 +0800

    django

---
+++ blog_project/uwsgi_test.py
@@ -0,0 +1,15 @@
+# test.py
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"Hello World"] # python3
+    #return ["Hello World"] # python2
+
+
+'''
+pip install uwsgi
+
+uwsgi --http :9000 --wsgi-file test.py
+启动uwsgi服务器后,在浏览器中输入地址  http//:47.97.165.75:9000
+如果服务正常   ,页面返回   Hello World
+
+'''
