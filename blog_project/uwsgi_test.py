0572638a624a9ffaf74348672ab44fb488003b70
gujientsy <gujientsy@163.com>
2018-09-03 17:04:38 +0800

    uptoprduct

--- blog_project/uwsgi_test.py
+++ blog_project/uwsgi_test.py
@@ -8,7 +8,7 @@ def application(env, start_response):
 '''
 pip install uwsgi
 
-uwsgi --http :9000 --wsgi-file test.py
+uwsgi --http :9000 --wsgi-file uwsgi_test.py
 启动uwsgi服务器后,在浏览器中输入地址  http//:47.97.165.75:9000
 如果服务正常   ,页面返回   Hello World
 
@@ -16,4 +16,6 @@ uwsgi --http :9000 --wsgi-file test.py
 '''
 https://blog.csdn.net/oolawokao/article/details/77994527
 
+nginx + uwsgi + django 原理:
+https://www.linuxidc.com/Linux/2017-03/141785.htm
 '''
