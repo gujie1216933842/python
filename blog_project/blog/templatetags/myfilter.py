52f7f09bf62097dcd37f0391e38525d53c8011e9
gujie1216933842 <gujientsy@163.com>
2018-09-09 20:30:39 +0800

    filter

--- blog_project/blog/templatetags/myfilter.py
+++ blog_project/blog/templatetags/myfilter.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 # 定义一个将日期中的阿拉伯数字转化为中文的过滤器
-
+# @register.filter  # 另一种注册的方式
 def month_to_upper(key):
     return ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'][key.month - 1]
 
