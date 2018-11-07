56890640287832949e9ac4a11e047f10ba769b21
gujie <jie.j.gu@rrd.com>
2018-11-07 17:14:00 +0800

    django

--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import User
 # Create your views here.
-
+from django.db.models import F
 def index(request):
-    list = User.objects.all()
-    content = {'list':list}
+
+    list = User.objects.get(name='aa')
     print(list)
-    return render(request,'booktest/index.html',content)
+    list.age = F('age')+ 1000
+    list.save()
+
+    return render(request,'booktest/index.html')
