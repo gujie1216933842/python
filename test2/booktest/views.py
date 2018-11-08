a353115d6c4c9be10b9e88e4bbcfe271c64ee09a
gujie <jie.j.gu@rrd.com>
2018-11-08 09:13:39 +0800

    django

--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .models import User
 # Create your views here.
 from django.db.models import F
+
+"""F()函数实例"""
 def index(request):
 
     list = User.objects.get(name='aa')
     print(list)
-    list.age = F('age')+ 1000
+    list.age = F('age') + 1000
     list.save()
 
     return render(request,'booktest/index.html')
