d38cccdfc436f1c8fccdbcaf9c5c47da9959cae9
gujie <jie.j.gu@rrd.com>
2018-11-15 14:37:07 +0800

    model

--- test2/booktest/models.py
+++ test2/booktest/models.py
@@ -7,7 +7,8 @@ class User(models.Model):
     name = models.CharField(max_length=50, verbose_name=u'姓名')
     age = models.IntegerField(max_length=11, verbose_name=u'年龄')
     hobby = models.CharField(max_length=20, verbose_name=u'爱好')
-    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+    add_time = models.DateTimeField(default=datetime.now, auto_now_add=True, verbose_name=u'添加时间')
+    update_time = models.DateTimeField(default=datetime.now, auto_now=True, verbose_name=u'更新时间')
 
     class Meta:
         verbose_name = u'用户'
--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -4,11 +4,21 @@ from .models import User
 from django.db.models import F
 
 """F()函数实例"""
-def index(request):
 
+
+def index(request):
     list = User.objects.get(name='aa')
     print(list)
     list.age = F('age') + 1000
     list.save()
 
-    return render(request,'booktest/index.html')
+    return render(request, 'booktest/index.html')
+
+
+"""Q()函数实例
+作用：对对象进行复杂查询，并支持&（and）,|（or），~（not）操作符。
+"""
+
+
+def test_q(request):
+    pass
