44fb5c6771c59dd11d83072eb8faa9428070abf9
gujie <jie.j.gu@rrd.com>
2018-11-15 14:47:34 +0800

    model

--- test2/booktest/models.py
+++ test2/booktest/models.py
@@ -5,10 +5,10 @@ from datetime import datetime
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=50, verbose_name=u'姓名')
-    age = models.IntegerField(max_length=11, verbose_name=u'年龄')
+    age = models.IntegerField(default=0, verbose_name=u'年龄')
     hobby = models.CharField(max_length=20, verbose_name=u'爱好')
-    add_time = models.DateTimeField(default=datetime.now, auto_now_add=True, verbose_name=u'添加时间')
-    update_time = models.DateTimeField(default=datetime.now, auto_now=True, verbose_name=u'更新时间')
+    add_time = models.DateTimeField( auto_now_add=True, verbose_name=u'添加时间')
+    update_time = models.DateTimeField( auto_now=True, verbose_name=u'更新时间')
 
     class Meta:
         verbose_name = u'用户'
